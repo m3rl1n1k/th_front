0e306b93a013c821ccd278f565c2d72db7b172c6
Bisix 21 <bisix21@gmail.com>
2025-06-21 18:39:52 +0000

    change login validation api to api/v1/login_check

--- src/config/urls.ts
+++ src/config/urls.ts
@@ -6,7 +6,7 @@ const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL || 'http://localhost:8
 
 export const URLS = {
   // Auth
-  login: '/api/login_check',
+  login: '/api/v1/login_check',
   register: `${API_BASE_URL}/auth/register`,
   logout: `${API_BASE_URL}/auth/logout`,
   me: `${API_BASE_URL}/user`,
